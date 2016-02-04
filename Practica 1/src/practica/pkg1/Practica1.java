4b1894322a6b52ee28dd113372f1e080f414d78f
Songoku <Songoku@Master_Lucy>
2016-02-03 19:53:25 -0600

    Interfaz

---
+++ Practica 1/src/practica/pkg1/Practica1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.pkg1;
+import FrontEnd.Interfaz;
+
+/**
+ *
+ * @author Songoku
+ */
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interfaz inter = new Interfaz();
+        inter.setLocationRelativeTo(null);
+        inter.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
